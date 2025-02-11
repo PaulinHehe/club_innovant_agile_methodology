5409b18eadadcd30476784d161f7eb05ab80e163
Paulin <124896613+PaulinHehe@users.noreply.github.com>
2025-02-11 15:32:42 +0100

    Récupération des instances de la classe Joueur

--- src/models/Database.java
+++ src/models/Database.java
@@ -5,11 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-    private static final String URL = "jdbc:mysql://localhost:3306/db_foot";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/db_foot?serverTimezone=UTC";
+    private static final String USER = "root";  // Remplace par ton utilisateur MySQL
+    private static final String PASSWORD = "";  // Mets ton mot de passe MySQL
 
     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Charge le driver MySQL
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver MySQL non trouvé", e);
+        }
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
--- src/models/JoueurModel.java
+++ src/models/JoueurModel.java
@@ -1,8 +1,52 @@
 package models;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import objets_bdd.Joueur;
+
 public class JoueurModel extends Model {
     public JoueurModel() {
         this.table = "joueurs";
         this.idField = "id";
+    }
+    
+    private Joueur getJoueur(ResultSet data) throws SQLException
+    {
+    	Joueur nj = new Joueur();
+		nj.id = data.getInt("id");
+		nj.nom = data.getString("nom");
+		nj.prenom = data.getString("prenom");
+		nj.dateNaissance = data.getTimestamp("dateNaissance");
+		nj.numeroLicence = data.getInt("numeroLicence");
+		nj.numeroMaillot = data.getInt("numeroMaillot");
+		nj.position = data.getString("position");
+		nj.taille = data.getFloat("taille");
+		nj.poids = data.getFloat("poids");
+		nj.piedFort = data.getString("piedFort");
+
+		return nj;
+    }
+
+    public Joueur get(int id) throws SQLException {
+    	ResultSet data = super.find(id);
+    	
+    	while(data.next())
+    		return this.getJoueur(data);
+    	return null;
+    }
+    
+    public List<Joueur> getAll() throws SQLException {
+    	ResultSet data = super.all();
+    	List<Joueur> retour = new ArrayList<Joueur>();
+    	while(data != null && data.next())
+    		retour.add(this.getJoueur(data));
+    	
+    	return retour;
     }
 }
