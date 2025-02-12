45d20d20958a9ada1cdb5501a63c56e05960ef24
Josias <djossoujosiashod@gmail.com>
2025-02-12 21:53:26 +0100

    Ajout du script bdd

--- script bdd/db_foot.sql
+++ script bdd/db_foot.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : mar. 11 fév. 2025 à 20:02
--- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.0.30
+-- Hôte : localhost
+-- Généré le : mer. 12 fév. 2025 à 21:45
+-- Version du serveur : 10.4.28-MariaDB
+-- Version de PHP : 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -45,7 +45,11 @@ INSERT INTO `consultations` (`id`, `details`, `resultats`, `traitements`, `idjou
 (2, 'Fatigue musculaire', 'Examen normal', 'Hydratation et repos', 2, 2),
 (3, 'Fracture du poignet', 'Plâtre 6 semaines', 'Immobilisation et rééducation', 3, 3),
 (4, 'Tendinite', 'Inflammation détectée', 'Physiothérapie recommandée', 4, 1),
-(5, 'Blessure aux ischio-jambiers', 'Déchirure partielle', 'Repos et renforcement musculaire', 5, 2);
+(5, 'Blessure aux ischio-jambiers', 'Déchirure partielle', 'Repos et renforcement musculaire', 5, 2),
+(6, 'Douleur au genou droit', 'Entorse légère', 'Repos 2 semaines', 1, 1),
+(7, 'Fatigue musculaire', 'Examen normal', 'Hydratation et repos', 2, 2),
+(8, 'Fracture du poignet', 'Plâtre 6 semaines', 'Immobilisation et rééducation', 3, 3),
+(9, 'Tendinite', 'Inflammation détectée', 'Physiothérapie recommandée', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -73,7 +77,13 @@ INSERT INTO `contrats` (`id`, `type`, `dateDebut`, `dateFin`, `salaire`, `primes
 (2, 1, '2023-06-30 22:00:00', '2027-06-29 22:00:00', 4000000, 800000, 'EUR', 2),
 (3, 1, '2021-06-30 22:00:00', '2025-06-29 22:00:00', 6000000, 1200000, 'EUR', 3),
 (4, 1, '2020-06-30 22:00:00', '2024-06-29 22:00:00', 7000000, 1500000, 'EUR', 4),
-(5, 1, '2021-06-30 22:00:00', '2025-06-29 22:00:00', 3500000, 500000, 'EUR', 5);
+(5, 1, '2021-06-30 22:00:00', '2025-06-29 22:00:00', 3500000, 500000, 'EUR', 5),
+(6, 1, '2022-06-30 22:00:00', '2026-06-29 22:00:00', 5000000, 1000000, 'EUR', 1),
+(7, 1, '2023-06-30 22:00:00', '2027-06-29 22:00:00', 4000000, 800000, 'EUR', 2),
+(8, 1, '2021-06-30 22:00:00', '2025-06-29 22:00:00', 6000000, 1200000, 'EUR', 3),
+(9, 1, '2020-06-30 22:00:00', '2024-06-29 22:00:00', 7000000, 1500000, 'EUR', 4),
+(10, 1, '2021-06-30 22:00:00', '2025-06-29 22:00:00', 3500000, 500000, 'EUR', 5),
+(11, 1, '2025-02-12 20:29:39', '0000-00-00 00:00:00', 2003000, 2000, 'EUR', 2);
 
 -- --------------------------------------------------------
 
@@ -102,7 +112,17 @@ INSERT INTO `donnees_medicales` (`id`, `allergies`, `vaccins`, `idConsultation`)
 (7, 'Fruits de mer', 'COVID-19, Varicelle', 2),
 (8, 'Aucune', 'Rougeole, Polio', 3),
 (9, 'Lait', 'Grippe, COVID-19', 4),
-(10, 'Aucune', 'Tétanos, Tuberculose', 5);
+(10, 'Aucune', 'Tétanos, Tuberculose', 5),
+(11, 'Pollen', 'Tétanos, Hépatite B', 1),
+(12, 'Aucune', 'COVID-19, Grippe', 2),
+(13, 'Arachides', 'Tétanos, Rougeole', 3),
+(14, 'Aucune', 'Hépatite B, Grippe', 4),
+(15, 'Gluten', 'COVID-19, Hépatite A', 5),
+(16, 'Poils de chat', 'Tétanos, Hépatite B', 1),
+(17, 'Fruits de mer', 'COVID-19, Varicelle', 2),
+(18, 'Aucune', 'Rougeole, Polio', 3),
+(19, 'Lait', 'Grippe, COVID-19', 4),
+(20, 'Aucune', 'Tétanos, Tuberculose', 5);
 
 -- --------------------------------------------------------
 
@@ -130,7 +150,12 @@ INSERT INTO `donnees_physiques` (`id`, `vitesseMax`, `distanceParcourue`, `nombr
 (2, 32.8, 9.3, 18, 58.7, 3.8, 650, 2),
 (3, 34.5, 11, 22, 61.5, 4.3, 720, 3),
 (4, 33.1, 8.9, 20, 57.4, 3.5, 680, 4),
-(5, 36, 12.2, 30, 62.8, 4.5, 750, 5);
+(5, 36, 12.2, 30, 62.8, 4.5, 750, 5),
+(6, 35.2, 10.5, 25, 60.2, 4.1, 700, 1),
+(7, 32.8, 9.3, 18, 58.7, 3.8, 650, 2),
+(8, 34.5, 11, 22, 61.5, 4.3, 720, 3),
+(9, 33.1, 8.9, 20, 57.4, 3.5, 680, 4),
+(10, 36, 12.2, 30, 62.8, 4.5, 750, 5);
 
 -- --------------------------------------------------------
 
@@ -156,7 +181,12 @@ INSERT INTO `donnees_psychologiques` (`id`, `profilPsychologique`, `suiviMental`
 (2, 'Anxieux sous pression', 'Besoin de suivi', 'Relaxation et gestion du stress', 'Suivi régulier', 2),
 (3, 'Très compétitif, mais impulsif', 'Risque d’épuisement', 'Coaching mental intensif', 'Deux séances en 2023', 3),
 (4, 'Calme et réfléchi', 'Stable mentalement', 'Visualisation des scénarios de match', 'Aucune consultation', 4),
-(5, 'Manque de confiance en soi', 'En cours de thérapie', 'Exercices de motivation et encouragements', 'Début de suivi en 2025', 5);
+(5, 'Manque de confiance en soi', 'En cours de thérapie', 'Exercices de motivation et encouragements', 'Début de suivi en 2025', 5),
+(6, 'Leader naturel, optimiste', 'Aucun trouble détecté', 'Méditation avant match', 'Consultation en 2024', 1),
+(7, 'Anxieux sous pression', 'Besoin de suivi', 'Relaxation et gestion du stress', 'Suivi régulier', 2),
+(8, 'Très compétitif, mais impulsif', 'Risque d’épuisement', 'Coaching mental intensif', 'Deux séances en 2023', 3),
+(9, 'Calme et réfléchi', 'Stable mentalement', 'Visualisation des scénarios de match', 'Aucune consultation', 4),
+(10, 'Manque de confiance en soi', 'En cours de thérapie', 'Exercices de motivation et encouragements', 'Début de suivi en 2025', 5);
 
 -- --------------------------------------------------------
 
@@ -193,7 +223,7 @@ INSERT INTO `joueurs` (`id`, `nom`, `prenom`, `dateNaissance`, `nationalite`, `n
 (8, 'Van Dijk', 'Virgil', '1991-07-07 22:00:00', 'Pays-Bas', 555666, 4, 'Défenseur', 1.93, 92, 'Droitier'),
 (9, 'Ramos', 'Sergio', '1986-03-29 23:00:00', 'Espagne', 777888, 4, 'Défenseur', 1.84, 82, 'Droitier'),
 (10, 'Alisson', 'Becker', '1992-10-01 22:00:00', 'Brésil', 999000, 1, 'Gardien', 1.91, 86, 'Droitier'),
-(11, 'Ter Stegen', 'Marc-André', '1992-04-29 22:00:00', 'Allemagne', 112233, 1, 'Gardien', 1.87, 85, 'Droitier'),
+(11, 'Ter Stegene', 'Marc-André', '1992-04-29 22:00:00', 'Allemagne', 112233, 1, 'Gardien', 1.87, 85, 'Droitier'),
 (12, 'Hakimi', 'Achraf', '1998-11-03 23:00:00', 'Maroc', 223344, 2, 'Défenseur', 1.81, 73, 'Droitier'),
 (13, 'Marquinhos', 'Marcos', '1994-05-13 22:00:00', 'Brésil', 334455, 5, 'Défenseur', 1.83, 79, 'Droitier'),
 (14, 'Robertson', 'Andrew', '1994-03-10 23:00:00', 'Écosse', 445566, 3, 'Défenseur', 1.78, 69, 'Gaucher'),
@@ -204,7 +234,24 @@ INSERT INTO `joueurs` (`id`, `nom`, `prenom`, `dateNaissance`, `nationalite`, `n
 (19, 'Foden', 'Phil', '2000-05-27 22:00:00', 'Angleterre', 990011, 47, 'Milieu', 1.71, 67, 'Gaucher'),
 (20, 'Pedri', 'González', '2002-11-24 23:00:00', 'Espagne', 101112, 8, 'Milieu', 1.74, 65, 'Droitier'),
 (41, 'Mbappé', 'Kylian', '1998-12-19 23:00:00', 'France', 123456, 7, 'Attaquant', 1.78, 73, 'Droitier'),
-(42, 'Mbappé', 'Kylian', '1998-12-19 23:00:00', 'France', 123456, 7, 'Attaquant', 1.78, 73, 'Droitier');
+(44, 'Messi', 'Lionel', '1987-06-23 22:00:00', 'Argentine', 789012, 10, 'Attaquant', 1.7, 72, 'Gaucher'),
+(45, 'Ronaldo', 'Cristiano', '1985-02-04 23:00:00', 'Portugal', 345678, 7, 'Attaquant', 1.87, 83, 'Droitier'),
+(46, 'Neymar', 'Junior', '1992-02-04 23:00:00', 'Brésil', 901234, 10, 'Ailier', 1.75, 68, 'Droitier'),
+(47, 'De Bruyne', 'Kevin', '1991-06-27 22:00:00', 'Belgique', 567890, 17, 'Milieu', 1.81, 70, 'Droitier'),
+(48, 'Modric', 'Luka', '1985-09-08 22:00:00', 'Croatie', 111222, 10, 'Milieu', 1.72, 66, 'Droitier'),
+(49, 'Kanté', 'N’Golo', '1991-03-28 23:00:00', 'France', 333444, 7, 'Milieu', 1.68, 65, 'Droitier'),
+(50, 'Van Dijk', 'Virgil', '1991-07-07 22:00:00', 'Pays-Bas', 555666, 4, 'Défenseur', 1.93, 92, 'Droitier'),
+(51, 'Ramos', 'Sergio', '1986-03-29 23:00:00', 'Espagne', 777888, 4, 'Défenseur', 1.84, 82, 'Droitier'),
+(52, 'Alisson', 'Becker', '1992-10-01 23:00:00', 'Brésil', 999000, 1, 'Gardien', 1.91, 86, 'Droitier'),
+(53, 'Ter Stegen', 'Marc-André', '1992-04-29 22:00:00', 'Allemagne', 112233, 1, 'Gardien', 1.87, 85, 'Droitier'),
+(54, 'Hakimi', 'Achraf', '1998-11-03 23:00:00', 'Maroc', 223344, 2, 'Défenseur', 1.81, 73, 'Droitier'),
+(55, 'Marquinhos', 'Marcos', '1994-05-13 22:00:00', 'Brésil', 334455, 5, 'Défenseur', 1.83, 79, 'Droitier'),
+(56, 'Robertson', 'Andrew', '1994-03-10 23:00:00', 'Écosse', 445566, 3, 'Défenseur', 1.78, 69, 'Gaucher'),
+(57, 'Lewandowski', 'Robert', '1988-08-20 22:00:00', 'Pologne', 556677, 9, 'Attaquant', 1.85, 81, 'Droitier'),
+(58, 'Griezmann', 'Antoine', '1991-03-20 23:00:00', 'France', 667788, 7, 'Attaquant', 1.76, 69, 'Gaucher'),
+(59, 'Benzema', 'Karim', '1987-12-18 23:00:00', 'France', 778899, 9, 'Attaquant', 1.85, 81, 'Droitier'),
+(60, 'Salah', 'Mohamed', '1992-06-14 22:00:00', 'Égypte', 889900, 11, 'Ailier', 1.75, 71, 'Gaucher'),
+(61, 'Foden', 'Phil', '2000-05-27 22:00:00', 'Angleterre', 990011, 47, 'Milieu', 1.71, 67, 'Gaucher');
 
 -- --------------------------------------------------------
 
@@ -232,7 +279,11 @@ INSERT INTO `performances` (`id`, `buts`, `date_match`, `passes_decisives`, `tir
 (2, 1, '2025-02-09 19:00:00', 2, 4, 8, 5, 0.9),
 (3, 3, '2025-02-08 16:30:00', 1, 6, 12, 7, 2.1),
 (4, 0, '2025-02-07 18:00:00', 0, 2, 6, 1, 0.3),
-(5, 1, '2025-02-06 14:00:00', 1, 3, 9, 4, 0.8);
+(5, 1, '2025-02-06 14:00:00', 1, 3, 9, 4, 0.8),
+(6, 2, '2025-02-10 17:00:00', 1, 5, 10, 3, 1.4),
+(7, 1, '2025-02-09 19:00:00', 2, 4, 8, 5, 0.9),
+(8, 3, '2025-02-08 16:30:00', 1, 6, 12, 7, 2.1),
+(10, 1, '2025-02-06 14:00:00', 1, 3, 9, 4, 0.8);
 
 -- --------------------------------------------------------
 
@@ -263,7 +314,18 @@ INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `username`, `mdp`, `role`) VA
 (7, 'Lefevre', 'Emma', 'elefevre', 'psymind', 'Psychologue du sport'),
 (8, 'Gomez', 'Hugo', 'hgomez', 'analyst2025', 'Analyste vidéo'),
 (9, 'Rossi', 'Marco', 'mrossi', 'scoutpro', 'Recruteur'),
-(10, 'Fernandez', 'Elena', 'efernandez', 'statsguru', 'Statisticien');
+(10, 'Fernandez', 'Elena', 'efernandez', 'statsguru', 'Statisticien'),
+(11, 'Dupont', 'Jean', 'jdupont', 'password123', 'Admin'),
+(12, 'Martin', 'Sophie', 'smartin', 'securepass', 'Médecin'),
+(13, 'Lopez', 'Carlos', 'clopez', 'football2025', 'Entraîneur'),
+(14, 'Dubois', 'Claire', 'cdubois', 'coachlife', 'Préparateur physique'),
+(15, 'Toure', 'Ahmed', 'atoure', 'mdpsuper', 'Médecin'),
+(16, 'Bernard', 'Lucas', 'lbernard', 'strongpass', 'Kinésithérapeute'),
+(17, 'Lefevre', 'Emma', 'elefevre', 'psymind', 'Psychologue du sport'),
+(18, 'Gomez', 'Hugo', 'hgomez', 'analyst2025', 'Analyste vidéo'),
+(19, 'Rossi', 'Marco', 'mrossi', 'scoutpro', 'Recruteur'),
+(20, 'Fernandez', 'Elena', 'efernandez', 'statsguru', 'Statisticien'),
+(21, 'Admin', 'Amin', 'admin.compte', '1234', 'Admin');
 
 --
 -- Index pour les tables déchargées
@@ -325,49 +387,49 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `consultations`
 --
 ALTER TABLE `consultations`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `contrats`
 --
 ALTER TABLE `contrats`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT pour la table `donnees_medicales`
 --
 ALTER TABLE `donnees_medicales`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT pour la table `donnees_physiques`
 --
 ALTER TABLE `donnees_physiques`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `donnees_psychologiques`
 --
 ALTER TABLE `donnees_psychologiques`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `joueurs`
 --
 ALTER TABLE `joueurs`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT pour la table `performances`
 --
 ALTER TABLE `performances`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
